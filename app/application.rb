16293ba8c229e2806b2fb5353a43cbb1703e58e6
Justine Bricknell <justinegayton@gmail.com>
2018-10-10 22:55:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,8 @@ class Application
     #puts "Current Time : " + time.inspect
     #resp.write "Time.local(*values)" #{}"#{time}\n"
     puts Time.local(values)
+    resp.write "#{Time.local(values)}\n"
+
     if time.hour < 11
       resp.write "Good Morning!"
     else time.hour > 12
