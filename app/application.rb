17016c5334914576efec6cce916488320232b490
Justine Bricknell <justinegayton@gmail.com>
2018-10-10 22:39:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time1 = Time.new
+    time = Time.new
     puts "Current Time : " + time1.inspect
 
-    resp.write "#{time1}\n"
+    resp.write "#{time.hour}\n"
 
-    if time1 < 12:00:00 
+    if time1 < 12:00:00
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
